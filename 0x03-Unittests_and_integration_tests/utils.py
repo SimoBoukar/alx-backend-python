edfb4e01a4549ab472499ca6c1931467351c93e1
Mohammed <6bouass9@gmail.com>
2024-08-28 00:12:00 +0100

    push test_utils tasks v3

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -35,8 +35,8 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
     """
     for key in path:
         if not isinstance(nested_map, Mapping):
-                raise KeyError(key)
-            nested_map = nested_map[key]
+            raise KeyError(key)
+        nested_map = nested_map[key]
 
     return nested_map
 
